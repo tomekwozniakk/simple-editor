b354fef657908162575828d0847355214ad2e2b7
Tomek Wozniak <tomekwozniakk@gmail.com>
2020-06-14 23:13:44 +0200

    added javascript to save and load

--- src/js/index.js
+++ src/js/index.js
@@ -6,4 +6,23 @@ import '../scss/main.scss';
 
 /* place your code below */
 
-console.log('HELLO 🚀')
+const entry = localStorage.getItem('entry');
+let result = '';
+
+ if (entry) {
+     result = entry;
+ }
+
+const save = document.querySelector('.save--js');
+const load = document.querySelector('.load--js');
+const textArea = document.querySelector('.form__area--js');
+textArea.value = result;
+
+save.addEventListener('click', () => {
+    localStorage.setItem('entry', textArea.value);
+} )
+
+load.addEventListener('click', () => {
+    localStorage.getItem('entry', textArea.value);
+    textArea.value = entry;
+})
